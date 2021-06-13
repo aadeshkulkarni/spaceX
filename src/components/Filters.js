8a59eae4aa3882cf10fbdd0aa8d6891dbba99bde
Aadesh Kulkarni <67855887+aadeshkulkarni@users.noreply.github.com>
2021-06-14 01:18:07 +0530

    Update Filters.js

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,7 +1,23 @@
 import React from "react"
 
 const Filters = (props) => {
-  const years = [2006, 2007, 2008, 2009, 2010, 2011]
+  const years = [
+    2006,
+    2007,
+    2008,
+    2009,
+    2010,
+    2011,
+    2012,
+    2013,
+    2014,
+    2015,
+    2016,
+    2017,
+    2018,
+    2019,
+    2020,
+  ]
   const successLaunch = ["True", "False"]
   const succcessLanding = ["True", "False"]
 
